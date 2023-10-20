e9216acb55853d92d61bab16953f53a51e1b49d6
denisyakimov07 <59424027+denisyakimov07@users.noreply.github.com>
2023-10-19 23:41:46 -0400

    fixted

--- dz_IA.py
+++ dz_IA.py
@@ -111,52 +111,13 @@ class Bot(BaseModel):
     Sets: List[Weapone]
 
 
-# with open("fileName.json", "r") as f:
-#     json_data = json.loads(f.read())
-# bot: Body = Body(**json_data)
-#
-# print(bot.model_dump_json(indent=4))
-
-with open("MMGPoliceLoadout.json", "r") as f:
-    json_data = json.loads(f.read())
-bot: Bot = Bot(**json_data)
-
-
-# for item in bot.InventoryCargo:
-#     print (item.ClassName)
-
-
-# for item in bot.InventoryAttachments:
-#     print (item)
-
-# print(bot)
-# print(bot.model_dump_json(indent=4))
 
 
 
 
 
 
-
-# CARGO_ITEMS=0
-# def add_cargo_iyem():
-#     CARGO_ITEMS =+ 1
-#
-#
-# add_cargo_item = [{'ClassName': item.ClassName, 'Chance': item.Chance} for item in bot.InventoryCargo]
-# print(add_cargo_item)
-#
-#
-# st.header('BotCargo', divider='rainbow')
-# dfColumns = st.columns(5)
-# for item in add_cargo_item:
-#     dfColumns[0].button('Remove', on_click=add_cargo_iyem)
-#     dfColumns[1].text_input('ClassName', value=item.get('ClassName'))
-#     # dfColumns[1].number_input(value=item.get('Chance'), label='Chance', key=f"{item.ClassName}Chance")
-#
-# # st.button('Add', on_click=add_cargo_iyem)
-#
-
+st.set_page_config(layout="wide")
 if "rows" not in st.session_state:
     st.session_state["rows"] = []
 
@@ -173,20 +134,19 @@ def remove_row(row_id):
 
 def generate_row(row_id):
     row_container = st.empty()
-    row_columns = row_container.columns([5,2,2,2,2,1])
+    row_columns = row_container.columns([5,1,1,1,1,1,1])
     class_name = row_columns[0].text_input("Class Name", key=f"class_name_{row_id}")
-    chans = row_columns[1].number_input("Chans", step=1, key=f"chans_{row_id}", value=1, )
-    QuantityMin = row_columns[2].number_input("QuantityMin", step=1, key=f"QuantityMin_{row_id}", value=0)
-    QuantityMax = row_columns[3].number_input("QuantityMax", step=1, key=f"QuantityMax_{row_id}", value=0)
-    HealthMin = row_columns[4].number_input("HealthMin", step=0.1, key=f"HealthMin_{row_id}", value=0)
-    HealthyMax = row_columns[5].number_input("HealthMax", step=0.1, key=f"HealthMax_{row_id}", value=0)
+    chans = row_columns[1].number_input("Chans", step=0.1, key=f"chans_{row_id}", value=1.0, min_value=0.0, max_value=1.0)
+    QuantityMin = row_columns[2].number_input("Quantity Min", step=0.1, key=f"QuantityMin_{row_id}", value=0.0, min_value=0.0, max_value=1.0)
+    QuantityMax = row_columns[3].number_input("Quantity Max", step=0.1, key=f"QuantityMax_{row_id}", value=0.0,min_value=0.0, max_value=1.0)
+    HealthMin = row_columns[4].number_input("Health Min", step=0.1, key=f"HealthMin_{row_id}", value=0.0, min_value=0.0, max_value=1.0)
+    HealthyMax = row_columns[5].number_input("Health Max", step=0.1, key=f"HealthMax_{row_id}", value=0.0, min_value=0.0, max_value=1.0)
     row_columns[6].button(f"🗑️", key=f"del_{row_id}", on_click=remove_row, args=[row_id])
-
     return {"class_name": class_name, "chans": chans, "QuantityMin": QuantityMin, "QuantityMax":QuantityMax,
             "HealthMin":HealthMin, "HealthyMax": HealthyMax}
 
 
-st.title("Item Inventory")
+st.title("Bot Cargo Inventory")
 
 for row in st.session_state["rows"]:
     row_data = generate_row(row)
